494bd272fa3d51b6b0139862ad2f6a6c2e22c1f0
the-gmt <the.gmt@gmail.com>
2026-03-30 16:18:53 -0400

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,12 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YBHHLMLVD1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  
+  let language = window.location.pathname.includes('/fr/') ? 'fr' : 'en';
+  
+  gtag('config', 'G-YBHHLMLVD1', {
+    'language': language
+  });
+</script>
